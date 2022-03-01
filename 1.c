60da515efe179833bc80d5273f0c92f3bfa1f66c
mting813018 <100187096+mting813018@users.noreply.github.com>
2022-03-01 14:43:23 +0800

    tsgh
    shgh

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    char b;
+    short c;
+    long d;
+
+    printf("%d\n", sizeof(a));
+    printf("%d\n", sizeof(b));
+    printf("%d\n", sizeof(c));
+    printf("%d\n", sizeof(d));
+}
