058f0d6c99a75ae3ae75ddc850d55e2195176cc2
mting813018 <100187096+mting813018@users.noreply.github.com>
2022-03-22 14:25:41 +0800

    print_variable

--- print_variable.c
+++ print_variable.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
     int b = 87;
     for (int j = 5; j < 23; j++)
-        {
-        b=87*j;
-        printf ("%d\n",b);
-        }
-}
+    }   
+     printf ("j=%d,b=%d\n",j,b*j);
+    }
+}   
